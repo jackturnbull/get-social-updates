6106963da5a246ca0bd7c5c8e5b8245b20f1fb26
Jack Turnbull <git@jack.codes>
2021-02-13 12:05:22 +0800

    feat: add blacklist, followed users

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 require("dotenv").config();
 const Twitter = require("twitter-lite");
 const TelegramBot = require("node-telegram-bot-api");
-const { pathOr } = require("ramda");
+const { any, compose, contains, equals, flip, gt, intersection, length, map, pathOr, prop, toLower } = require("ramda");
 
-const GET_TWITTER_USER = "getprotocol";
-const GET_TWITTER_TAG = "get";
+const TWITTER_USERS = ["getprotocol", "bloemersmaarten"];
+const TWITTER_SYMBOLS = ["get"];
+const TWITTER_BLACKLIST = ["huobi"];
+const MAXIMUM_SYMBOLS_COUNT = 4;
 
 const telegramBot = new TelegramBot(process.env.TELEGRAM_TOKEN);
-
-const client = new Twitter({
+const twitterClient = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-const tweetContainsGetSymbol = (symbols) => symbols.find(({ text }) => text.toLowerCase() === GET_TWITTER_TAG);
-const tweetContainsGetUserMention = (userMentions) => userMentions.find(({ screen_name: screenName }) => screenName.toLowerCase() === GET_TWITTER_USER);
-const isRetweet = (tweet) => !!tweet.retweeted_status;
-const isTweetTruncated = (tweet) => !!tweet.truncated;
-const isTweetValid = (tweet) => tweetContainsGetSymbol(getSymbols(tweet)) || tweetContainsGetUserMention(getUserMentions(tweet));
-
+// Getters
+const getContent = (tweet) => tweet.full_text || pathOr(tweet.text, ["extended_tweet", "full_text"], tweet);
 const getSymbols = pathOr([], ["entities", "symbols"]);
 const getUserMentions = pathOr([], ["entities", "user_mentions"]);
+const getUserName = pathOr("N/A", ["user", "screen_name"]);
+
+// Content checkers
+const tweetContainsSymbol = (symbols) =>
+  gt(length(intersection(map(compose(toLower, prop("text")), symbols), TWITTER_SYMBOLS)), 0);
+const tweetContainsUserMention = (userMentions) =>
+  gt(length(intersection(map(compose(toLower, prop("screen_name")), userMentions), TWITTER_USERS)), 0);
+const tweetFromUser = (screenName) => any(equals(toLower(screenName)), TWITTER_USERS);
+
+// Boolean flags
+const isRetweet = (tweet) => !!tweet.retweeted_status;
+const isTweetTruncated = (tweet) => !!tweet.truncated;
+
+// Composed selectors
+const doesTweetContainBlacklist = (tweet) => any(flip(contains)(toLower(getContent(tweet))), TWITTER_BLACKLIST);
+const doesTweetContainTooManySymbols = (tweet) => gt(length(getSymbols(tweet)), MAXIMUM_SYMBOLS_COUNT);
+const doesTweetContainReference = (tweet) =>
+  tweetFromUser(getUserName(tweet)) ||
+  tweetContainsSymbol(getSymbols(tweet)) ||
+  tweetContainsUserMention(getUserMentions(tweet));
 
 const pushToTelegram = (tweet) => {
   const { id_str: id } = tweet;
 
-  const tweeterName = pathOr("N/A", ["user", "screen_name"], tweet);
-  const tweetContent = tweet.full_text || pathOr(tweet.text, ["extended_tweet", "full_text"], tweet);
+  const tweetContent = getContent(tweet);
+  const tweeterName = getUserName(tweet);
   const messageText = `<a href="https://twitter.com/${tweeterName}"><strong>@${tweeterName}</strong></a>: ${tweetContent}`;
 
   telegramBot.sendMessage(process.env.TELEGRAM_CHANNEL, messageText, {
@@ -59,19 +76,29 @@ const processTweet = async (tweet) => {
     return;
   }
 
-  if (!isTweetTruncated(tweet) && isTweetValid(tweet)) {
+  if (doesTweetContainTooManySymbols(tweet)) {
+    console.log("[INFO] tweet contains too many symbols with ID", id);
+    return;
+  }
+
+  if (doesTweetContainBlacklist(tweet)) {
+    console.log("[INFO] blacklisted tweet with ID", id);
+    return;
+  }
+
+  if (!isTweetTruncated(tweet) && doesTweetContainReference(tweet)) {
     console.log("[INFO] tweet complete from stream, pushing to telegram", id);
     pushToTelegram(tweet);
     return;
   }
 
   console.log("[INFO] fetching extended tweet with ID", id);
-  const extendedTweet = await client.get("statuses/show", {
+  const extendedTweet = await twitterClient.get("statuses/show", {
     id,
     tweet_mode: "extended",
   });
 
-  if (isTweetValid(extendedTweet)) {
+  if (doesTweetContainReference(extendedTweet)) {
     console.log("[INFO] pushing extended tweet to telegram", id);
     pushToTelegram(extendedTweet);
     return;
@@ -86,9 +113,12 @@ const errorHandler = (e) => {
   process.exit(1);
 };
 
-client
+const trackUsers = TWITTER_USERS.map((user) => `@${user}`);
+const trackSymbols = TWITTER_SYMBOLS.map((symbol) => `$${symbol}`);
+
+twitterClient
   .stream("statuses/filter", {
-    track: `@${GET_TWITTER_USER},$${GET_TWITTER_TAG}`,
+    track: [...trackUsers, ...trackSymbols].join(","),
   })
   .on("data", (data) => {
     try {
